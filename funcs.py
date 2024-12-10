04000ea0acd74a4d0e40066d92ddc63ffab90ea6
Marina <hyunahanna21@gmail.com>
2024-12-10 17:51:23 +0700

    Сумма массивов

--- funcs.py
+++ funcs.py
@@ -32,7 +32,19 @@ def generation_arr(size):
         mat.append(randint(-10, 10))
     return mat
 
+def sum_arr(mat_1, mat_2, size):
+    mat_rezult = []
+    for i in range(size):
+        if mat_1[i] == mat_2[i]:
+            mat_rezult.append(0)
+        else:
+            mat_rezult.append(mat_1[i] + mat_2[i])
+    return mat_rezult
+
 if __name__ == "__main__":
     print(is_int(23423), is_int("dlfj"), is_int("3452"))
-    print(input_arr(3))
-    print(generation_arr(3))
+    mat_1 = input_arr(3)
+    print(mat_1)
+    mat_2 = generation_arr(3)
+    print(mat_2)
+    print(sum_arr(mat_1, mat_2, 3))
