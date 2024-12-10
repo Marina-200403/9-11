012da3eb18cb9839fe9eadf3ee6b85c01c8a04f8
Marina <hyunahanna21@gmail.com>
2024-12-10 17:44:24 +0700

    Генерация массива

--- funcs.py
+++ funcs.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def is_int(s):
     try:
         if type(s) is int:
@@ -24,6 +26,13 @@ def input_arr(size):
             print ('try again')
     return mat
 
+def generation_arr(size):
+    mat = []
+    for i in range(size):
+        mat.append(randint(-10, 10))
+    return mat
+
 if __name__ == "__main__":
     print(is_int(23423), is_int("dlfj"), is_int("3452"))
     print(input_arr(3))
+    print(generation_arr(3))
